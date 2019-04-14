8e13a11e231cc9e4c6a9846c585f8944c6320235
Jaeuk Cho <chicuk9001@gmail.com>
2019-04-15 00:03:01 +0900

    constructor inheritance

---
+++ constructor-inheritance.js
@@ -0,0 +1,30 @@
+function Person(name, first,  second) {
+    this.name = name;
+    this.first = first;
+    this.second = second;
+}
+
+Person.prototype.sum = function () {
+    return this.first + this.second;
+}
+
+function PersonPlus (name, first, second, third) {
+    Person.call(this, name, first, second);
+    this.third = third;
+}
+
+// 부모 이어주기
+//PersonPlus.prototype.__proto__ = Person.prototype;
+
+PersonPlus.prototype = Object.create(Person.prototype);
+
+PersonPlus.prototype.avg = function () {
+    return (this.first + this.second + this.third) / 3;
+}
+
+
+
+var kim = new PersonPlus('kim', 10, 20 ,30);
+console.log("kim.sum()", kim.sum());
+console.log("kim.avg()", kim.avg());
+console.log("kim.constructor", kim.constructor);
