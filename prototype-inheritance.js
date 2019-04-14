2e340e72cf038d84e35b90a070dca0239ea5989d
Jaeuk Cho <chicuk9001@gmail.com>
2019-04-14 23:22:53 +0900

    prototype inheritance

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,11 +1,16 @@
 var superObj = {
     superVal:'super'
 }
-var subObj = {
-    subVal:'sub'
-}
 
-subObj.__proto__ = superObj; // prototype, __proto__
+var subObj = Object.create(superObj);
+subObj.subVal = 'sub';
+//debugger;
+
+// var subObj = {
+//     subVal:'sub'
+// }
+
+//subObj.__proto__ = superObj; // prototype, __proto__
 
 console.log('subObj.subVal =>', subObj.subVal);
 console.log('subObj.superVal =>', subObj.superVal);
@@ -13,3 +18,30 @@ console.log('subObj.superVal =>', subObj.superVal);
 subObj.superVal = 'sub';
 console.log('superObj.superVal =>', superObj.superVal);
 
+var kim = {
+    name:'kim',
+    first:10,
+    second:20,
+    sum:function() {
+        return this.first + this.second
+    }
+}
+
+var lee = Object.create(kim);
+lee.name = 'lee';
+lee.first = 10;
+lee.second = 20;
+lee.avg = function () {
+    return (this.first + this.second) / 2;
+}
+
+// var lee = {
+//     name:'lee',
+//     first:10,
+//     second:10
+// }
+
+// lee.__proto__ = kim;
+
+console.log('lee.sum() : ', lee.sum());
+console.log('lee.avg() : ', lee.avg());
