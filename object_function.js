2a0211e82d266e807d96507ebfecb62e7250b056
Jaeuk Cho <chicuk9001@gmail.com>
2019-04-14 23:33:11 +0900

    bind function

--- object_function.js
+++ object_function.js
@@ -16,3 +16,6 @@ function sum (prefix) {
 
 console.log("sum.call(kim)", sum.call(kim, 10));
 console.log("sum.call(lee)", sum.call(lee, 20));
+
+var kimSum = sum.bind(kim, 30);
+console.log('kimSum()', kimSum());
