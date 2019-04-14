7a32a9b7360187b50bc8d6e59d9a5b0072562771
Jaeuk Cho <chicuk9001@gmail.com>
2019-04-14 23:29:27 +0900

    call function

---
+++ object_function.js
@@ -0,0 +1,18 @@
+var kim = {
+    name:'kim',
+    first:10,
+    second:20
+}
+
+var lee = {
+    name:'lee',
+    first:10,
+    second:10
+}
+
+function sum (prefix) {
+    return prefix + (this.first + this.second);
+}
+
+console.log("sum.call(kim)", sum.call(kim, 10));
+console.log("sum.call(lee)", sum.call(lee, 20));
