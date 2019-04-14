0242cd66b7191329b6bb242a5b9c2973d00d73a3
Jaeuk Cho <chicuk9001@gmail.com>
2019-04-14 22:52:09 +0900

    super

--- inheritance.js
+++ inheritance.js
@@ -5,19 +5,28 @@ class Person {
         this.second = second;
     }
 
-    sum() {
+    sum () {
         return this.first + this.second;
     }
 }
 
 class PersonPlus extends Person {
+    constructor(name, first, second, third) {
+        super(name, first, second);
+        this.third = third;
+    }
+
+    sum () {
+        return super.sum() + this.third;
+    }
+
     avg () {
-        return (this.first + this.second) / 2;
+        return (this.first + this.second + this.third) / 3;
     }
 }
 
 
-var kim = new PersonPlus('kim', 10, 20);
+var kim = new PersonPlus('kim', 10, 20, 30);
 console.log('kim', kim.sum());
 console.log('kim', kim.avg());
 
