f6daab0d534790df73ea6fa9b9c289533649da84
Jaeuk Cho <chicuk9001@gmail.com>
2019-04-14 22:04:09 +0900

    create constructor function

--- object_factory.js
+++ object_factory.js
@@ -1,22 +1,16 @@
-var kim = {
-    name:'kim',
-    first:10,
-    second:20,
-    third:30,
-    sum:function () {
+function Person (name, first, second, third) {
+    this.name = name;
+    this.first = first;
+    this.second = second;
+    this.third = third;
+    this.sum = function () {
         return this.first + this.second + this.third;
     }
 }
 
-var lee = {
-    name:'lee',
-    first:10,
-    second:10,
-    third:10,
-    sum:function () {
-        return this.first + this.second + this.third;
-    }
-}
+// constructor(앞에 new가 붙으면 생성자 함수가 됨.) 생성자의 필요성 : 초기화
+var kim = new Person('kim', 10, 20, 30);
+var lee = new Person('lee', 10, 10, 10);
 
 console.log("kim.sum()", kim.sum());
 console.log("lee.sum()", lee.sum());
@@ -25,3 +19,6 @@ console.log("lee.sum()", lee.sum());
 var d1 = new Date('2019-4-14');
 console.log('d1.getFullYear()', d1.getFullYear());
 console.log('d1.getMonth()', d1.getMonth());
+
+
+console.log('Date', Date);
